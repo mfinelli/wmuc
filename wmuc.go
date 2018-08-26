f64a1781c998a867b5e3db0d7d1f42da6cd3ef0d
Mario Finelli <mario@finel.li>
2018-08-25 20:58:25 -0400

    Parse input from chuckfile

--- wmuc.go
+++ wmuc.go
@@ -1,13 +1,20 @@
 package main
 
+import "errors"
 import "fmt"
+import "io/ioutil"
+import "os"
 
 import "github.com/mfinelli/wmuc/parser"
 
 func main() {
-	input := `# this is a comment
-	repo "test one"
-	repo 'test two'`
+	input, err := readChuckfile()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	results := parser.Parse(input)
 
 	for _, project := range results {
@@ -17,4 +24,18 @@ func main() {
 			fmt.Printf("\trepo: %s\n", repo.Url)
 		}
 	}
+}
+
+func readChuckfile() (string, error) {
+	allowed := []string{"chuckfile", "Chuckfile", "CHUCKFILE"}
+
+	for _, filename := range allowed {
+		bytes, err := ioutil.ReadFile(filename)
+
+		if err == nil {
+			return string(bytes), nil
+		}
+	}
+
+	return "", errors.New("unable to load a chuckfile in this directory")
 }
