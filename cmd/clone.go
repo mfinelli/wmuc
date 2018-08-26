1977f439ffa93c3fc2392efd857c59b8acfc5b02
Mario Finelli <mario@finel.li>
2018-08-25 22:08:08 -0400

    Enable repository clone

--- cmd/clone.go
+++ cmd/clone.go
@@ -2,16 +2,41 @@ package cmd
 
 import "fmt"
 import "os"
+import "path"
 import "path/filepath"
+import "strings"
 
+import "gopkg.in/src-d/go-git.v4"
 import "github.com/mfinelli/wmuc/parser"
 
 func CloneRepos(results map[string]parser.Project) {
+	cwd, _ := os.Getwd()
+
 	for _, project := range results {
 		createDirectory(project.Path)
+		os.Chdir(filepath.Join(cwd, project.Path))
 
 		for _, repo := range project.Repos {
-			fmt.Printf("\trepo: %s\n", repo.Url)
+			repopath := path.Base(repo.Url)
+			repodir := strings.TrimSuffix(repopath,
+				filepath.Ext(repopath))
+
+			if _, err := os.Stat(repodir); os.IsNotExist(err) {
+				_, err := git.PlainClone(repodir, false,
+					&git.CloneOptions{
+						URL:      repo.Url,
+						Progress: os.Stdout,
+					})
+
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+			}
+		}
+
+		if project.Path != "" {
+			os.Chdir(cwd)
 		}
 	}
 }
