d03205acd0c5cf3cc737eb5f0aad02b6d11abd8e
Mario Finelli <mario@finel.li>
2018-08-25 21:42:24 -0400

    Create top-level project directories

--- cmd/clone.go
+++ cmd/clone.go
@@ -1,14 +1,31 @@
 package cmd
 
 import "fmt"
+import "os"
+import "path/filepath"
+
 import "github.com/mfinelli/wmuc/parser"
 
 func CloneRepos(results map[string]parser.Project) {
 	for _, project := range results {
-		fmt.Printf("For project: %q\n", project.Path)
+		createDirectory(project.Path)
 
 		for _, repo := range project.Repos {
 			fmt.Printf("\trepo: %s\n", repo.Url)
 		}
+	}
+}
+
+func createDirectory(path string) {
+	if path == "" {
+		return
+	}
+
+	cwd, _ := os.Getwd()
+	err := os.MkdirAll(filepath.Join(cwd, path), 0755)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
